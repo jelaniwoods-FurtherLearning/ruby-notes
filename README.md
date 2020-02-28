fa5f49d4506870d98f5b51d21d97e56237088640
Jelani Woods <jelani@firstdraft.com>
2020-02-28 16:46:38 -0600

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby Notes
+
+I'm reading through Learn Ruby the Hard Way and maybe Chris Pine's Ruby book for the first time and want to take notes on stuff!
---
+++ the_hard_way/ex13.rb
@@ -0,0 +1,7 @@
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+
+# run like ex13.rb first 2nd 3rd
